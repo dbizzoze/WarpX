e0ecf24ca68614e84782a8fe8b4d9df575d20c08
dbizzoze <dbizzoze@slac.stanford.edu>
2023-10-23 14:54:55 -0700

    Updated comments and added Solenoid.H code

--- Source/AcceleratorLattice/LatticeElements/Solenoid.cpp
+++ Source/AcceleratorLattice/LatticeElements/Solenoid.cpp
@@ -25,21 +25,12 @@ Solenoid::AddElement (amrex::ParmParse & pp_element, amrex::ParticleReal & z_loc
     AddElementBase(pp_element, z_location);
 
     amrex::ParticleReal scale = 0._prt;
-    amrex::ParticleReal freq = 0._prt;
-    amrex::ParticleReal theta = 0._prt;
-    amrex::Vector<amrex::ParticleReal> e_coef = {};
     amrex::Vector<amrex::ParticleReal> b_coef = {};
 
     utils::parser::queryWithParser(pp_element, "scale", scale);
-    utils::parser::queryWithParser(pp_element, "freq", freq);
-    utils::parser::queryWithParser(pp_element, "theta", theta);
-    utils::parser::queryArrWithParser(pp_element, "e_coef", e_coef);
     utils::parser::queryArrWithParser(pp_element, "b_coef", b_coef);
 
     h_scale.push_back(scale);
-    h_freq.push_back(freq);
-    h_theta.push_back(theta);
-    h_e_coef.push_back(e_coef);
     h_b_coef.push_back(b_coef);
 
 }
@@ -51,15 +42,7 @@ Solenoid::WriteToDevice ()
 
     d_scale.resize(h_scale.size());
     amrex::Gpu::copyAsync(amrex::Gpu::hostToDevice, h_scale.begin(), h_scale.end(), d_scale.begin());
-    d_freq.resize(h_freq.size());
-    amrex::Gpu::copyAsync(amrex::Gpu::hostToDevice, h_freq.begin(), h_freq.end(), d_freq.begin());
-    d_theta.resize(h_theta.size());
-    amrex::Gpu::copyAsync(amrex::Gpu::hostToDevice, h_theta.begin(), h_theta.end(), d_theta.begin());
 
-    d_e_coef.resize(h_e_coef.size());
-    for (auto i = 0lu; i < h_e_coef.size(); ++i) {
-        amrex::Gpu::copyAsync(amrex::Gpu::hostToDevice, h_e_coef[i].begin(), h_e_coef[i].end(), d_e_coef[i].begin());
-    }
     d_b_coef.resize(h_b_coef.size());
     for (auto i = 0lu; i < h_e_coef.size(); ++i) {
         amrex::Gpu::copyAsync(amrex::Gpu::hostToDevice, h_b_coef[i].begin(), h_b_coef[i].end(), d_b_coef[i].begin());
@@ -87,12 +70,6 @@ SolenoidDevice::InitSolenoidDevice (Solenoid const& h_solenoid)
     d_zs_arr = h_solenoid.d_zs.data();
     d_ze_arr = h_solenoid.d_ze.data();
     d_scale_arr = h_solenoid.d_scale.data();
-    d_freq_arr = h_solenoid.d_freq.data();
-    d_theta_arr = h_solenoid.d_theta.data();
-    for (auto i = 0lu; i < nelements; ++i) {
-        d_num_e_coef[i] = h_solenoid.d_e_coef[i].size();
-        d_e_coef_arr[i] = h_solenoid.d_e_coef[i].data();
-    }
     for (auto i = 0lu; i < nelements; ++i) {
         d_num_b_coef[i] = h_solenoid.d_b_coef[i].size();
         d_b_coef_arr[i] = h_solenoid.d_b_coef[i].data();
